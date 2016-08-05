326c9fb632f914e49231bba1ecb6ae43c7a3c639
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-08-04 21:45:44 -0300

    Update aes256.rb

--- aes256.rb
+++ aes256.rb
@@ -1,5 +1,7 @@
 require "openssl"
 
+#puts OpenSSL::Cipher.ciphers
+
 data = "Very, very confidential data"
 
 cipher = OpenSSL::Cipher::AES.new(256, :CFB)
@@ -8,6 +10,7 @@ key = cipher.random_key
 iv = cipher.random_iv
 
 encrypted = cipher.update(data) + cipher.final
+puts encrypted
 
 decipher = OpenSSL::Cipher::AES.new(256, :CFB)
 decipher.decrypt
@@ -15,5 +18,6 @@ decipher.key = key
 decipher.iv = iv
 
 plain = decipher.update(encrypted) + decipher.final
+puts plain
 
 puts data == plain
