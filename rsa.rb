9b5a31391f6a4ba20f0b607683a5b97fd49980ce
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-08-09 21:00:38 -0300

    Update rsa.rb

--- rsa.rb
+++ rsa.rb
@@ -3,7 +3,7 @@ require 'openssl'
 key = OpenSSL::PKey::RSA.new(2048)
 
 puts "\nEncrypt: " 
-puts encrypted_string = key.public_encrypt('Very, very confidential data', OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+p encrypted_string = key.public_encrypt('Very, very confidential data', OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
 
 puts "\nDecrypt: "
-puts decrypted_string = key.private_decrypt(encrypted_string, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+p decrypted_string = key.private_decrypt(encrypted_string, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
