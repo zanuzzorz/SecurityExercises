ba4aec6ad3f805bd5ea8db3554b40a6ce75bee45
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-08-30 20:47:54 -0300

    Update cesar.rb

--- Criptografia-Classica/cesar.rb
+++ Criptografia-Classica/cesar.rb
@@ -1,15 +1,17 @@
 cifra = msg_cifrada = msg_decifrada = ''
-mensagem = "Very, very confidential data"
-chave = 7
+
+mensagem = File.read('1.input')
+chave = 17
 
 mensagem.each_char { |x|
-	cifra = ((x.ord + chave) % 256).chr
-	msg_cifrada +=  cifra
+	cifra = ((x.ord + chave) % 256)
+	msg_cifrada <<  cifra
 }
-puts "\nMensagem cifrada: \n" + msg_cifrada.to_s
+#puts "\nMensagem cifrada: \n" + msg_cifrada.to_s
+print msg_cifrada.to_s
 
 msg_cifrada.each_char { |x|
-	cifra = ((x.ord - chave) % 256).chr
-	msg_decifrada +=  cifra
+	cifra = ((x.ord - chave) % 256)
+	msg_decifrada <<  cifra
 }
-puts "\nMensagem decifrada: \n" + msg_decifrada.to_s
+#puts "\nMensagem decifrada: \n" + msg_decifrada.to_s
