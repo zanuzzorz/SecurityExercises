368139a98123e1e528be2f14c2a83344f69b1c38
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-08-31 17:29:09 -0300

    Update cesar.rb

--- Criptografia-Classica/cesar.rb
+++ Criptografia-Classica/cesar.rb
@@ -1,17 +1,18 @@
 cifra = msg_cifrada = msg_decifrada = ''
 
-mensagem = File.read('1.input')
+mensagem = File.open('1.input','rb')
 chave = 17
 
 mensagem.each_char { |x|
-	cifra = ((x.ord + chave) % 256)
-	msg_cifrada <<  cifra
+	cifra = ((x.ord + chave) % 256).chr
+	msg_cifrada +=  cifra
 }
 #puts "\nMensagem cifrada: \n" + msg_cifrada.to_s
 print msg_cifrada.to_s
 
 msg_cifrada.each_char { |x|
-	cifra = ((x.ord - chave) % 256)
-	msg_decifrada <<  cifra
+	cifra = ((x.ord - chave) % 256).chr
+	msg_decifrada +=  cifra
 }
 #puts "\nMensagem decifrada: \n" + msg_decifrada.to_s
+
