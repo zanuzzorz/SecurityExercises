e4a889e1eb93a2c181d13ada38be63dda4e66805
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-09-08 15:16:38 -0300

    Update vigenere.rb

--- Criptografia-Classica/vigenere.rb
+++ Criptografia-Classica/vigenere.rb
@@ -1,20 +1,21 @@
-mensagem = 'ATACARBASESUL'
-chave = 'LIMAO'
+# mensagem = 'ATACARBASESUL'
+mensagem = File.open("#{ARGV[0]}",'rb')
+chave = "#{ARGV[1]}"
 
-BASE = 'A'.ord
-SIZE = 'Z'.ord - BASE + 1
-
-def cifrar(texto, chave, dir)
-	texto = texto.upcase.gsub(/[^A-Z]/, '')
-	msg_chave = chave.upcase.gsub(/[^A-Z]/, '').chars.map{|c| c.ord - BASE}.cycle
-	texto.each_char.inject('') do |msg_cifrada, char|
-	  offset = msg_chave.next
-	  msg_cifrada << ((char.ord - BASE).send(dir, offset) % SIZE + BASE).chr
-	end
+def vig_enc(msg, key)
+  msg.each_byte.zip(key.each_byte.cycle).map{|p, k| ((p + k ) % 256)}.map(&:chr).join
 end
- 
-msg_cifrada = cifrar(mensagem, chave, :+)
-puts "Mensagem cifrada: #{msg_cifrada}"
 
-msg_decifrada  = cifrar(msg_cifrada, chave, :-)
-puts "Mensagem decifrada: #{msg_decifrada}"
+def vig_dec(msg, key)
+  msg.each_byte.zip(key.each_byte.cycle).map{|p, k| ((p - k ) % 256)}.map(&:chr).join
+end
+
+# msg_cifrada = cifrar(mensagem, chave, :+)
+# puts "Mensagem cifrada: #{msg_cifrada}"
+msg_cifrada = vig_enc(mensagem, chave)
+print msg_cifrada
+
+if ARGV[2] == 'd'
+	msg_decifrada  = vig_dec(msg_cifrada, chave)
+	puts "Mensagem decifrada: #{msg_decifrada}"
+end
