9e59ddbd5ddd420c3249c8a35812c762c1e5944c
zanuzzorz <zanuzzorz@gmail.com>
2016-09-15 20:07:19 -0300

    Ajustes na Cifra de transposição

--- Criptografia-Classica/transposicao.rb
+++ Criptografia-Classica/transposicao.rb
@@ -1,6 +1,6 @@
 def crypt(mensagem, chave)
 	msg_cifrada = Array.new(chave, '')
-	mensagem += '#' until mensagem.size % chave == 0
+	mensagem += '¬' until mensagem.size % chave == 0
 	mensagem = mensagem.scan(/#{'.' * chave}/)  
 
 	mensagem.size.times { |x|
@@ -11,7 +11,7 @@ def crypt(mensagem, chave)
 end
 
 def decrypt(mensagem, chave)
-	tamanho = (mensagem.tr('#', '').size % chave).to_i
+	tamanho = (mensagem.tr('¬', '').size % chave).to_i
 
 	msg_decifrada = Array.new((mensagem.size / chave), '')
 	mensagem = mensagem.scan(/#{'.' * (mensagem.size / chave)}/)  
@@ -20,11 +20,20 @@ def decrypt(mensagem, chave)
     	msg_decifrada.size.times { |i|  msg_decifrada[i] += mensagem[x][i] }
  	}	
 
- 	[msg_decifrada].transpose.join('').tr('#', '').tr('&', ' ')
+ 	[msg_decifrada].transpose.join('')
 end
 
-mensagem = 'Very, very confidential data'
-chave = 3
+def ajustar_texto(texto)
+	texto.tr('¬', '').tr('¢', ' ').tr('£', "\n")
+end
 
-puts msg_cifrada = crypt(mensagem.tr(' ', '&'), chave)
-puts decrypt(msg_cifrada, chave)
+mensagem = File.open("#{ARGV[0]}",'rb').read
+mensagem = mensagem.tr(' ', '¢').tr("\n", '£')   # Subtitui os espaços em branco e as quebras de linha por simbolos.
+chave = "#{ARGV[1]}".to_i
+
+
+msg_cifrada = crypt(mensagem, chave)
+print ajustar_texto(msg_cifrada)
+
+msg_decifrada = decrypt(msg_cifrada, chave)
+#print ajustar_texto(msg_decifrada)
