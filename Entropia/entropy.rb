fc0d33600d760ea948865a149fc87e047efdff7b
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-10-06 19:11:45 -0300

    Update entropy.rb

--- Entropia/entropy.rb
+++ Entropia/entropy.rb
@@ -1,16 +1,14 @@
-texto =  ARGV[0] ? File.open("#{ARGV[0]}",'rb').read : "texto_de_exemplo"
-base  =  ARGV[1] ? "#{ARGV[1]}".to_i : 256
-
-freq = {}
-texto.each_char  { |c| 
-	x = texto.count c
-	freq.update({c => x})
-}
-puts "Frequencia: #{freq}"
-
-res = 0
-freq.each do |key, value|
-	prob = value.to_f / (texto.size).to_f
-	res += prob * Math.log(prob, base)
+def frequency(text)
+	h = Hash.new
+	text.split("").each {|c| h[c] = text.count(c) if not h.has_key?(c)}
+	return h
 end
-puts "Entropia: #{-res}"
+
+def entropy(text, freq,  base)
+	res = 0
+	freq.each do |key, value|
+		prob = value.to_f / (text.size).to_f
+		res += prob * Math.log(prob, base)
+	end
+	-res
+end
