da1f9817b67c10d9a8b7ebc306924dee09a8cf40
zanuzzorz <zanuzzorz@gmail.com>
2016-10-13 23:30:22 -0300

    Quebra cifra de transposição por Força Bruta

--- Criptografia-Classica/Forca-Bruta/transposicao.rb
+++ Criptografia-Classica/Forca-Bruta/transposicao.rb
@@ -27,11 +27,14 @@ for chave in 1..10**6
     palavras = []
     lista_palavras.each{ |word| palavras << word if words.include? word }
 
-    g = palavras.size > 250 && escuro.size > 5000
-    m = palavras.size > 25 && escuro.size > 100 && escuro.size < 5000
-    p = palavras.size > 5 && escuro.size < 100
+    gg = palavras.size > 999 && escuro.size > 50000
+    g  = palavras.size > 250 && escuro.size > 5000 && escuro.size < 50000
+    m  = palavras.size > 25  && escuro.size > 100  && escuro.size < 5000
+    p  = palavras.size > 5   && escuro.size  > 10  && escuro.size < 100
+    pp = palavras.size > 2   && escuro.size  < 10
+    i  =  palavras.size == escuro.size
 
-    if p ||  m || g
+    if pp || p ||  m || g || gg || i
         File.open('output', 'w').write(msg)
         puts "Chave: #{chave}"
         break
