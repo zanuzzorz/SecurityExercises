d80c0e1910e2a01b2aaf0908053c65208a77b9cb
Ricardo Zanuzzo <zanuzzorz@gmail.com>
2016-10-23 22:56:48 -0200

    Validações para quebra da cifra de vigenere

--- Criptografia-Classica/Forca-Bruta/vigenere.rb
+++ Criptografia-Classica/Forca-Bruta/vigenere.rb
@@ -1,16 +1,21 @@
 lista = [*('a'..'z'), *('0'..'9')]
+alfabeto = [*('a'..'z')]
+numeros = [*('0'..'9')]
 texto = File.open("#{ARGV[0]}",'rb').read
 words = open('words.txt', 'rb').read.split
 comum = [] 
 chave = ''
 
+alfabeto.each {|n| comum << [n]}
+numeros.each {|n| comum << [n]}
+alfabeto.each {|n| alfabeto.each {|l| comum << [l, n]}}
+numeros.each {|n| numeros.each {|l| comum << [l, n]}}
+alfabeto.each {|n| numeros.each {|l| comum << [l, n]}}
+lista.each {|n| comum << [n]}
 lista.each {|n| lista.each {|l| comum << [l, n]}}
-# comum = words
 
 comum.each { |i|
-
     i.each { |j| chave += j }
-    # chave = i
 
     msg_decifrada = texto.each_byte.zip(chave.each_byte.cycle).map{|p, k| ((p - k ) % 256)}.map(&:chr).join
     lista_palavras = msg_decifrada.split()
@@ -18,10 +23,16 @@ comum.each { |i|
     palavras = []
     lista_palavras.each{ |word| palavras << word if words.include? word }
 
-    if palavras.size > 50
+    gg  = palavras.size > 999 && texto.size > 50000
+    g   = palavras.size > 250 && texto.size > 5000 && texto.size < 50000
+    m   = palavras.size > 25  && texto.size > 100  && texto.size < 5000
+    p   = palavras.size > 5   && texto.size > 10  && texto.size < 100
+    pp  = palavras.size > 2   && texto.size < 10
+    ig  =  palavras.size == texto.size
+
+    if pp || p ||  m || g || gg || ig
         File.open('output', 'wb').write(msg_decifrada)
         puts "Chave: #{i}"
         break
     end
 }
-
